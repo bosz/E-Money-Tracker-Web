5988d5ce1f6320daede2316684fdadb130143383
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-10-16 20:51:38 +0100

    Added the page title for every route

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 import './../styles/App.css';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { AdminLayout } from '../components';
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+
+  const [titleText, setTitleText] = useState('');
+  useEffect(() => {
+    switch (pathname) {
+      case '/':
+        setTitleText('Home');
+        break;
+      case '/transactions':
+        setTitleText('Transactions');
+        break;
+      case '/kiosks':
+        setTitleText('Kiosks');
+        break;
+      case '/commercial-agents':
+        setTitleText('Commercial agents');
+        break;
+      case '/zones':
+        setTitleText('Zones');
+        break;
+      default:
+        break;
+    }
+  }, [pathname]);
+
+  console.log(pathname);
   return (
     <>
       <Head>
@@ -16,7 +44,7 @@ export default function MyApp({ Component, pageProps }) {
         />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       </Head>
-      <AdminLayout>
+      <AdminLayout pageTitle={titleText}>
         <Component {...pageProps} />
       </AdminLayout>
     </>
